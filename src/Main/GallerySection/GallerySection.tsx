3fe906d8784ce28898171b11ca82bd2499a15c37
Sergio <sergiofabian.madrid@hotmail.com>
2025-06-09 00:03:34 -0600

    Implemets responsive design to tablets

--- src/Main/GallerySection/GallerySection.tsx
+++ src/Main/GallerySection/GallerySection.tsx
@@ -4,6 +4,9 @@ function GallerySection() {
     return(
         <section className="gallery-section" id="gallery">
             <h2>Gallery section</h2>
+            <div className='more-pictures'>
+                <a href="">Do you want to see more?</a>
+            </div>
         </section>
     );
 }
